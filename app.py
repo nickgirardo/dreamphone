db739927b569e42ca025dd62ef88a3f8b661355b
Josh Romer <jromer94@gmail.com>
2016-04-17 08:25:59 -0400

    added audio for hints

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 import random
+import json
  
 app = Flask(__name__)
 
+#Key: Session Phone number, Value: Dreamboys number
 session = {} 
+
+with open('Dream_Boys.json') as data_file:    
+    people = json.load(data_file) 
 
 phone = {
     "+19732644177": "Joe",
@@ -21,15 +26,21 @@ phone = {
     "+16093035331": "Biggie",
 }
 
+categories = {
+    'location': ['bar', 'cave', 'commune'],
+    'food': ['pizza', 'nachos', 'wings', 'dumplings'],
+    'travel': ['skateboard', 'bus', 'car', 'bike'],
+    'language': ['ruby', 'python', 'javascript', 'go'],
+}
+
+base_url = "https://raw.githubusercontent.com/nickgirardo/dreamphone/master/audio/final/"
+
 @app.route("/voice", methods=['GET', 'POST'])
 def voice_incoming():
     """Respond to incoming requests."""
     target = request.values.get('To', None)
     caller = request.values.get('From', None)
-    print target
-    print caller
-    #should check for none?
-    #session[None] is valid?
+    
     resp = twilio.twiml.Response()
 
     if caller not in session:
@@ -37,8 +48,17 @@ def voice_incoming():
         print session[caller]
         resp.say("Welcome to Dreamphone! Find your dream boy!")
 
-    resp.gather(numDigits=1, action='/guess', timeout=5)
-    resp.say("This is a hint")
+    target_profile = people[phone[target]]
+    dream_profile = people[phone[session[caller]]]
+
+    category = target_profile['set']
+
+    options = [x for x in categories[category] if x != dream_profile[category]]
+
+    choice = random.choice(options)
+
+    resp.gather(numDigits=1, action='/guess', timeout=2)
+    resp.play(get_clue(choice, phone[target]))
  
     return str(resp)
 
@@ -63,6 +83,11 @@ def guess():
         resp.say("Nice Try, but guess again")
 
     return str(resp)
+
+def get_clue(choice, person):
+    url = base_url  + str(person).lower() + "/no_" + str(choice) + ".wav"
+    print url
+    return url 
  
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
