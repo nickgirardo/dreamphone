5accdd76bc814e9d86570910f046996eeeeec9a6
Nick Girardo <nick@nbp-189-186.nbp.ruw.rutgers.edu>
2016-04-17 02:57:31 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+
+phone = {
+    "+19732644177": "Joe",
+    "+19733060490": "Eric",
+    "+19735247011": "Rich",
+    "+19733308109": "Lars",
+    "+17323477185": "Tobias",
+    "+17323477216": "Eddie",
+    "+17322035203": "Shahan",
+    "+17322109837": "Swift",
+    "+16097265347": "Sam",
+    "+16092514356": "Nick",
+    "+16097265313": "Josh",
+    "+16093035331": "Biggie",
+}
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice_incoming():
+    """Respond to incoming requests."""
+    target = request.values.get('To', None)
+    caller = request.values.get('From', None)
+    print target
+    print caller
+    resp = twilio.twiml.Response()
+    resp.say("Ayy Lmao")
+ 
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
